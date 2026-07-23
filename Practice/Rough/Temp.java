614f0f84a4d5ea9c5b134772646863b6da92957c
Ashutosh Singh <ashutosh.singh1998.as@gmail.com>
2026-07-23 18:15:27 +0530

    Add Fibonacci Series Question

--- Practice/Rough/Temp.java
+++ Practice/Rough/Temp.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
+//The Fibonacci series is a sequence of numbers where each number is the sum of the two numbers before it.
+
 public class Temp {
     public static void main(String[] args) 
     {
         Scanner sc=new Scanner(System.in);
-        int[][] arr=new int[4][3];
-        for(int row=0;row<arr.length;row++)
+        System.out.print("Enter a number upto which you want to find Fibonacci Series :");
+        int n=sc.nextInt();
+        int x=0,y=1;
+        int fib=0;
+        System.out.print("Fibonacci Series upto "+n+"= "+x+","+y+",");
+        for(int i=1;i<=n;i++)
         {
-            for(int column=0;column<arr[row].length;column++)
-            {
-                System.out.print("Enter element of array: ");
-                arr[row][column]=sc.nextInt();
-            }
+            fib=(x+y);
+            System.out.print(fib+",");
+            x=y;
+            y=fib;
         }
-        System.out.println("Your Array is:");
-        for(int row=0;row<arr.length;row++)
-        {
-            for(int column=0;column<arr[row].length;column++)
-            {
-                System.out.print(arr[row][column]+"  ");
-            }
-            System.out.println();
-        }
+        System.out.print(".....");
         sc.close();
     }
     
