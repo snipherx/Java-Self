365d957128156eda0a5c868aae93f4965bfab1b0
Ashutosh Singh <ashutosh.singh1998.as@gmail.com>
2026-07-23 15:24:47 +0530

    DoWhile Loop

---
+++ Practice/2.Looping Statements/DoWhileLoop.java
@@ -0,0 +1,16 @@
+//It A do-while loop is a looping statement that executes the block of code at least once,
+//and then checks the condition. If the condition is true, it repeats the loop;
+// otherwise, it stops.
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int a=1;
+    do
+    {
+        System.out.println("Do While Loop Run atleast once if condition is wrong");
+        a++;
+    }
+    while(a<1);
+    
+    }
+}
