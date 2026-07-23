cff4c0af75db8bceaf505eebf431bea4e8d65473
Ashutosh Singh <ashutosh.singh1998.as@gmail.com>
2026-07-23 18:18:35 +0530

    Fibonacci Series

--- Java Ques/FibonacciSeries.java
+++ Java Ques/FibonacciSeries.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
 public class FibonacciSeries {
-
-    
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number upto which you want to find Fibonacci Series :");
+        int n=sc.nextInt();
+        int x=0,y=1;
+        int fib=0;
+        System.out.print("Fibonacci Series upto "+n+"= "+x+","+y+",");
+        for(int i=1;i<=n;i++)
+        {
+            fib=(x+y);
+            System.out.print(fib+",");
+            x=y;
+            y=fib;
+        }
+        System.out.print(".....");
+        sc.close();
+    }
 }
