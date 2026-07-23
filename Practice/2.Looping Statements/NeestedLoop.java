a2878ae78d6f91ab7e11a52fadc3dd195a31d3ca
Ashutosh Singh <ashutosh.singh1998.as@gmail.com>
2026-07-23 15:31:42 +0530

    Nested Loop

---
+++ Practice/2.Looping Statements/NeestedLoop.java
@@ -0,0 +1,15 @@
+//Loop inside loop
+
+public class NeestedLoop {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+    {
+        for(int x=1;x<=i;x++)
+        {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    }
+    
+}
