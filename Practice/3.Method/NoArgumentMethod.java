483ba19fb213168243cd372c948f382ee97cd84f
Ashutosh Singh <ashutosh.singh1998.as@gmail.com>
2026-07-23 16:56:40 +0530

    No Argument Method

---
+++ Practice/3.Method/NoArgumentMethod.java
@@ -0,0 +1,10 @@
+public class NoArgumentMethod {
+    public static void task()
+    {
+        System.out.println("Try No-Argument Method also known as Non-Parameterized Method");
+    }
+    public static void main(String[] args) {
+        task();
+    }
+    
+}
