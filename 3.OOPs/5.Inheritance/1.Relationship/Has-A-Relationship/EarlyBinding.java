486f2147f63846014ff035826675a6446d72d4b8
Ashutosh Singh <ashutosh.singh1998.as@gmail.com>
2026-08-19 06:40:23 +0530

    Has-A-Relationship Early Binding

---
+++ 3.OOPs/5.Inheritance/1.Relationship/Has-A-Relationship/EarlyBinding.java
@@ -0,0 +1,26 @@
+class Engine {
+
+    void start() {
+        System.out.println("Engine starts");
+    }
+}
+
+class Car {
+
+    //Engine e = new Engine();   // Car HAS-A Engine
+
+    void drive() {
+        Engine e = new Engine();
+        e.start();             // Early binding
+        System.out.println("Engine='on' --> Now we can Drive");
+    }
+}
+
+public class EarlyBinding {
+    public static void main(String[] args) {
+
+        Car c = new Car();
+
+        c.drive();
+    }
+}
