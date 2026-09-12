410c2a5bf4ed2883277f33ddbb02820dc371b74c
Ashutosh Singh <ashutosh.singh1998.as@gmail.com>
2026-09-12 22:59:52 +0530

    no msg

--- Rough/Demo.java
+++ Rough/Demo.java
@@ -1,24 +1,12 @@
-/**
- * Demo
- */
-public class Demo {
-    String firstName;
-    String lastName;
-    @Override 
-    public int hashCode()
-    {
-        return firstName.hashCode()+lastName.hashCode();
-    }
-    Demo(String firstName, String lastName)
-    {
-        this.firstName=firstName;
-        this.lastName=lastName;
-    }
-
-    public static void main(String[] args) {
-        HashCodeExperiment d1=new HashCodeExperiment("Ashutosh", "Singh");
-        HashCodeExperiment d2=new HashCodeExperiment("Ashutosh", "Singh");
-        System.out.println("HashCode of d1: "+d1.hashCode());
-        System.out.println("HashCode of d2: "+d2.hashCode());
+import java.util.Scanner;
+class Demo{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        System.out.println("Your name is "+name+" and age is "+age);
+        sc.close();
     }
 }
